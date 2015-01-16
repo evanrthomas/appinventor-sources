a5252897e93c5c6e9e09a0087a88795d4fb0fbba
Evan Thomas <evanrthom@gmail.com>
2015-01-16 00:35:13 -0500

    visjs example incorporated into manage shared pages overlay

--- appinventor/appengine/customGWTjs/modal.js
+++ appinventor/appengine/customGWTjs/modal.js
@@ -1,20 +1,4 @@
-window.exported = window.exported || {};
-window.exported.openSharedPagesOverlay =  function() {
-  var width = 900;
-  var height = 600;
-  document.getElementById("canvas").width =  width;
-  document.getElementById("canvas").height =  height - 100;
-  var c = document.getElementById("canvas");
-  var ctx = c.getContext("2d");
-  var pages = [];
-  var pageWidth = 140;
-  var pageHeight = 1.3*140;
-  var error = new Image();
-  error.src = "images/error.png";
-  error.setAttribute('width', '12px');
-  error.setAttribute('height', '12px');
-
-  var drawPage = function(x,y,title, color, components) {
+var drawPage = function(x,y,title, color, components) {
     pages.push({i:pages.length, x:x, y:y, title:title});
     ctx.beginPath();
     ctx.lineWidth="5";
@@ -40,7 +24,6 @@ window.exported.openSharedPagesOverlay =  function() {
       } else {
         comp = components[i];
       }
-      //console.log('alpha');
 
       var img = new Image();
       img.src ='images/' + comp + '.png';
@@ -53,77 +36,65 @@ window.exported.openSharedPagesOverlay =  function() {
       ctx.stroke();
       ctx.restore();
     }
-  }
-
-  function connect(pagei, pagej, pos1, pos2) {
-    var fromX;
-    var fromY;
-    function set(pagei, pos1) {
-      var fromX;
-      var fromY;
-      if (pos1 == 'l') {
-        fromX = pages[pagei].x;
-        fromY = pages[pagei].y + pageHeight/2;
-      } else if (pos1 == 'r') {
-        fromX = pages[pagei].x + pageWidth;
-        fromY = pages[pagei].y + pageHeight/2;
-      } else if(pos1 == 't') {
-        fromX = pages[pagei].x + pageWidth/2;
-        fromY = pages[pagei].y;
-      } else if(pos1 == 'b') {
-        fromX = pages[pagei].x +pageWidth/2;
-        fromY = pages[pagei].y + pageHeight;
-      }
-      return {x:fromX, y:fromY};
-    }
-    var from = set(pagei, pos1);
-    var to = set(pagej, pos2);
-    ctx.beginPath();
-    ctx.strokeStyle = "black";
-    ctx.moveTo(from.x, from.y);
-    ctx.lineTo(to.x, to.y);
-    ctx.stroke();
-  }
+}
 
 
-  (function fullProject() {
-    drawPage(5, 5, "screen1", undefined, ["label", "button", "button", "image", "listPicker"]);
-    drawPage(55, 255, "screen2", undefined, ["canvas", "clock"]);
-    drawPage(358, 38, "screen3", undefined, ["label", "label", "label", "button", "notifier"]);
-
-
-    drawPage(708, 30, "library_page_1", "green");
-    drawPage(289, 308, "shared_page_1", "green");
-    drawPage(657, 295, "library_page_2", "green");
-
-    connect(0, 4, 'r', 't');
-    connect(1, 4, 'r', 'l');
-    connect(2, 4, 'b', 't');
-    connect(2, 3, 'r', 'l');
-    connect(4, 5, 'r', 'l');
-  });
-  (function screen3() {
-      drawPage(10, 200, "screen3", undefined, ["label", "label", "label", "button", "notifier"]);
-      drawPage(400, 10, "library_page_1", "green");
-      drawPage(380, 300, "shared_page_1", "green", [["label", false], ["button", true]]);
-      connect(0, 2, 'r', 'l');
-      connect(0, 1, 'r', 'l');
-    })();
-
- document.getElementById('fade').style.display = 'block';
- document.getElementById('overlay').style.display = 'block';
- document.getElementById('new_shared_page_btn').onclick = window.exported.newSharedPage
-
- var clearOverlay = function() {
-   document.getElementById('fade').style.display = 'none';
+var clearOverlay = function() {
+  document.getElementById('fade').style.display = 'none';
   document.getElementById('overlay').style.display = 'none';
- }
- document.getElementById('fade').addEventListener("click", clearOverlay);
+}
 
- window.onkeyup = function(e) { //TODO (evan): I think this will overwrite any other onkeyup functions, make it add instead of replace
-   if (e.keyCode == 27) { //escape key code
-     clearOverlay();
-   }
-   //TODO (evan): remove the event click listeners you just added so you don't get a million listeners
- }
+var openSharedPagesOverlay =  function() {
+
+  (function initializeOverlay() {
+    document.getElementById('fade').style.display = 'block';
+    document.getElementById('overlay').style.display = 'block';
+    document.getElementById('new_shared_page_btn').onclick = window.exported.newSharedPage;
+    document.getElementById('fade').addEventListener("click", clearOverlay);
+
+    window.onkeyup = function(e) { //TODO (evan): I think this will overwrite any other onkeyup functions, make it add instead of replace
+      if (e.keyCode == 27) { //escape key code
+        clearOverlay();
+      }
+      //TODO (evan): remove the event click listeners you just added so you don't get a million listeners
+    }
+  })();
+
+
+  var width = 900;
+  var height = 600;
+  var pageWidth = 140;
+  var pageHeight = 1.3*140;
+
+
+  // create an array with nodes
+  var nodes = [
+    {id: 1, label: 'Node 1'},
+    {id: 2, label: 'Node 2'},
+    {id: 3, label: 'Node 3'},
+    {id: 4, label: 'Node 4'},
+    {id: 5, label: 'Node 5'}
+  ];
+
+  // create an array with edges
+  var edges = [
+    {from: 1, to: 2},
+    {from: 1, to: 3},
+    {from: 2, to: 4},
+    {from: 2, to: 5}
+  ];
+
+  // create a network
+  var container = document.getElementById('vis_network_area');
+  var data = {
+    nodes: nodes,
+    edges: edges
+  };
+  debugger;
+  var network = new vis.Network(container, data, {});
+
+
 };
+
+window.exported = window.exported || {};
+window.exported.openSharedPagesOverlay = openSharedPagesOverlay;
