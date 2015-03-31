0680f28c5bb2a9fb851979a748904bfa5c6dccca
Evan Thomas <evanrthom@gmail.com>
2015-03-31 03:25:14 -0400

    Helper changes

--- appinventor/appengine/src/com/google/appinventor/client/helper/Helper.java
+++ appinventor/appengine/src/com/google/appinventor/client/helper/Helper.java
@@ -1,35 +1,40 @@
 package com.google.appinventor.client.helper;
 
+import com.google.appinventor.client.editor.youngandroid.YaCodePageEditor;
+import com.google.appinventor.client.editor.youngandroid.YaSharedPageEditor;
 import com.google.appinventor.client.output.OdeLog;
 import com.google.gwt.core.client.JavaScriptObject;
 
 import java.util.Collection;
 
 public class Helper {
-  private static int tablevel  = 0;
 
-  public static void indent() {
-    tablevel += 1;
+  public static native void indent() /*-{
+    $wnd.console.group();
+  }-*/;
+
+  public static void indent(String msg) {
+    Helper.println(msg);
+    Helper.indent();
   }
 
-  public static void unindent() {
-    tablevel -= 1;
-    if (tablevel < 0) {
-      tablevel = 0;
-    }
-  }
 
-  private static String repeat(String s, int n) {
-    String s2 = "";
-    for (int i = 0; i<n; i++) {
-      s2 += s;
-    }
-    return s2;
+  public static native void unindent() /*-{
+    $wnd.console.groupEnd();
+  }-*/;
+
+  public static native void groupCollapsed() /*-{
+     $wnd.console.groupCollapsed();
+  }-*/;
+
+  public static void groupCollapsed(String msg) {
+    Helper.println(msg);
+    Helper.groupCollapsed();
   }
 
   public static void println(String s) {
-    consolePrint(repeat("\t", tablevel) + s);
-    OdeLog.log(repeat("\t", tablevel) + s);
+    consolePrint(s);
+    OdeLog.log(s);
   }
 
   public static<T> void println(Collection<T> items) {
@@ -40,6 +45,15 @@ public class Helper {
     println(s);
   }
 
+  public static<T> void println(String msg, Collection<T> items) {
+    println(msg);
+    println(items);
+  }
+
+
+  public static native void consolePrint(JavaScriptObject s) /*-{
+     $wnd.console.log(s);
+    }-*/;
 
   public static native void consolePrint(String s) /*-{
      $wnd.console.log(s);
@@ -65,4 +79,32 @@ public class Helper {
   public static native String jsonToString(JavaScriptObject obj) /*-{
     return JSON.stringify(obj);
   }-*/;
+
+  public static native String jsonToString(JavaScriptObject obj, int tabs) /*-{
+    return JSON.stringify(obj, undefined, tabs);
+  }-*/;
+
+  public static String getProjectName(YaCodePageEditor editor) {
+    return editor.getProjectRootNode().getName();
+
+  }
+
+  public static String editorDescriptor(YaCodePageEditor editor) {
+    return editor.getProjectId() + "_"  + editor.getName();
+  }
+
+
+  public static void printChildrenDescriptor(Collection<YaSharedPageEditor> children) {
+    Helper.println("childrenDescriptor " + children.size());
+    Helper.indent();
+    for (YaSharedPageEditor child: children) {
+      Helper.println(child.getProjectId() + "_" + child.getName());
+    }
+     Helper.unindent();
+  }
+
+  public static void printChildrenDescriptor(String message, Collection<YaSharedPageEditor> children) {
+    Helper.println(message);
+    Helper.printChildrenDescriptor(children);
+  }
 }
